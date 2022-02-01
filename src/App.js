e5e54bf9296767254f57cf586187b587a7c63b6c
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2022-02-01 17:37:20 -0500

    fixed routing issues now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 import RecipeForm from './components/RecipeForm';
 import Login from './login-logout/Login';
+import HomePage from './homepage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <RecipeForm />
-      <Login/>
+      <NavLink exact to ='/homepage'>
+        Home
+      </NavLink>
+      <NavLink exact to ='/login'>
+        Login
+      </NavLink>
+      <NavLink exact to='/recipe-form'>
+        Add Recipe
+      </NavLink>   
+      <Routes>
+        <Route exact path='/homepage' element={<Login />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/homepage' element={<HomePage />} />
+        <Route exact path='/recipe-form' element={<RecipeForm />}/>
+      </Routes>
     </div>
   );
 }
